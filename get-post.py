aa9e0b71359bc0cc21a34ce2c777527695299ed1
Edridge D'Souza <edsouza@umass.edu>
2016-09-02 11:46:30 -0400

    Modularized main component of get-post script

--- get-post.py
+++ get-post.py
@@ -7,50 +7,58 @@
 # From there, you can try to solve the problem within its own directory
 
 # Hard-code these in for quick eval. Add in argparse when functional
-challengeNumber = 255
-difficultyNumber = 3
 
 import praw
 import re
 #import argparse
 
-user_agent = "Edridge D'Souza get-post script for /r/dailyprogrammer"
-r = praw.Reddit(user_agent=user_agent)
-sub = 'dailyprogrammer'
-
-def BuildQuery(ch, diff):
-    challenge = u'Challenge #' + str(ch)
-    difficulty = {
-        1: u'easy',
-        2: u'(med OR intermediate)',
-        3: u'(hard OR difficult)'    
-    }[diff]
-    return challenge + ' ' + difficulty   
-query = BuildQuery(challengeNumber, difficultyNumber)
-
-# Return most relevant submissions to search, then double-check titles
-subsearch = [i for i in r.search(query, subreddit = sub, sort = u'relevance')]
-
-# Double-check post title contains right challenge number and difficulty
-def VerifyPost(redditobject, ch, diff):
-    title = redditobject.title.lower()
-    rightChallenge = bool(re.search(str(ch), title))
-    difficulty = {
-        1: u'easy',
-        2: u'(medium|intermediate)',
-        3: u'(hard|difficult)'
-    }[diff]
+def GetPost(challengeNumber, difficultyNumber):
+    user_agent = "Edridge D'Souza get-post script for /r/dailyprogrammer"
+    r = praw.Reddit(user_agent=user_agent)
+    sub = 'dailyprogrammer'
     
-    rightDiff = bool(re.search(str(difficulty), title))
-    return rightChallenge and rightDiff
+    def BuildQuery(ch, diff):
+        challenge = u'Challenge #' + str(ch)
+        difficulty = {
+            1: u'easy',
+            2: u'(med OR intermediate)',
+            3: u'(hard OR difficult)'    
+        }[diff]
+        return challenge + ' ' + difficulty   
+    query = BuildQuery(challengeNumber, difficultyNumber)
     
-truePosts = [i for i in subsearch if VerifyPost(i, challengeNumber, difficultyNumber)]
-
-if len(truePosts) == 1:
-    post = truePosts[0]
-    title, url, body = post.title, post.url, post.selftext
-else:
-    print("Ambiguous search. Please try again with clear parameters.")
+    # Return most relevant submissions to search, then double-check titles
+    subsearch = [i for i in r.search(query, subreddit = sub, sort = u'relevance')]
     
+    # Double-check post title contains right challenge number and difficulty
+    def VerifyPost(redditobject, ch, diff):
+        title = redditobject.title.lower()
+        rightChallenge = bool(re.search(str(ch), title))
+        difficulty = {
+            1: u'easy',
+            2: u'(medium|intermediate)',
+            3: u'(hard|difficult)'
+        }[diff]
+        
+        rightDiff = bool(re.search(str(difficulty), title))
+        return rightChallenge and rightDiff
+        
+    truePosts = [i for i in subsearch if VerifyPost(i, challengeNumber, difficultyNumber)]
+    
+    if len(truePosts) == 1:
+        post = truePosts[0]
+        title, url, body = post.title, post.url, post.selftext
+    else:
+        print("Ambiguous search. Please try again with clear parameters.")
+        return False # Just exit the function before anything gets done
+    
+    
+#==============================================================================
+# Execution
+#==============================================================================
+if __name__ == "__main__":
+    challengeNumber = 255
+    difficultyNumber = 3
+    GetPost(challengeNumber, difficultyNumber)
 
     
