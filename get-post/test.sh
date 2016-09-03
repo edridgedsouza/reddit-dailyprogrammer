9b9f9035e92ca5878d35f98bff0244a1b8216e68
Edridge D'Souza <edsouza@umass.edu>
2016-09-02 21:29:29 -0400

    Comments

--- get-post/test.sh
+++ get-post/test.sh
@@ -1,9 +1,12 @@
 #! /usr/bin/env bash
+#
 echo "Starting test"
 #
 # Won't work for the first 2 challenge numbers
 # Because of the different title naming conventions
 # Expect it to work for Challenge #3 onwards
+# I could just write a loop for this, but let's be honest.
+# No one likes writing loops in bash.
 #
 ./get-post.py 1 1
 ./get-post.py 1 2
